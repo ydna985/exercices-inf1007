f2aa38f1e22982de3f5c69d2381aa43f58b10e52
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2024-09-22 22:03:46 -0400

    something

--- ch03_2/exercice.py
+++ ch03_2/exercice.py
@@ -22,20 +22,35 @@ def point_in_circle(point, circle_center, circle_radius):
 	# TODO: Retourner vrai si le point est à l'intérieur du cercle, faux sinon.
 	# point[0] et circle_center[0] pour accéder au X
 	# point[1] et circle_center[1] pour accéder au Y
-	pass
+	x=point[0]-circle_center[0]
+	y=point[1]-circle_center[1]
+	lenght=math.sqrt(x**2+y**2)
+	if(lenght > circle_radius):
+		return False
+	else:
+		return True
 
 def cash(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$, 25¢, 10¢ et 5¢ à remettre pour représenter la valeur. Il faut arrondir à la pièce de 5¢ près. Il faut remplir les variables twenties, tens, fives, ones, quarters, dimes et nickels avec le quantité de chaque dénomination.
 
-	twenties=value//20
-	reste=value%20
+	non_decimal_value=int(value)
+	twenties=non_decimal_value//20
+	reste=non_decimal_value%20
 	tens=reste//10
 	reste%=10
 	fives=reste//5
 	reste%=5
-	twos=reste//2
-	ones=reste%2
-	return (twenties, tens, fives, twos, ones)
+	ones=reste
+	
+	decimal_value=value%1
+	print(decimal_value)
+	twentifive_cents=round(decimal_value//0.25)
+	decimal_value%=0.25
+	ten_cents=round(decimal_value//0.10)
+	decimal_value%=0.10
+	five_cents=decimal_value//0.05
+
+	return (twenties, tens, fives, ones, twentifive_cents, ten_cents, five_cents)
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres.
@@ -43,10 +58,11 @@ def format_base(value, base, digit_letters):
 	result = ""
 	abs_value = abs(value)
 	while abs_value != 0:
-		pass
+		result+=digit_letters[abs_value%16]
+		abs_value=abs_value//16
 	if value < 0:
 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
+		result="-"+result
 	return result
 
 
